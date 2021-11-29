938e7a021943d4b8b3b8fc16790c9c73c0ffcb1c
unknown <oficialbeto23@gmail.com>
2021-11-29 09:36:06 -0300

    first commit

---
+++ src/test/java/com/sistemabancario/model/ContaTest.java
@@ -0,0 +1,80 @@
+package com.sistemabancario.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ContaTest {
+    @Test
+    void testSetNumeroValido(){
+        final Conta instance = new Conta();
+        final String esperado = "12345-6";
+        instance.setNumero(esperado);
+        final String obtido = instance.getNumero();
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testSetNumeroInvalidoNaoArmazena(){
+        final Conta instance = new Conta();
+        final String invalido ="123";
+        assertThrows(IllegalArgumentException.class, () -> instance.setNumero(invalido));
+        final String obtido = instance.getNumero();
+        assertNotEquals(invalido, obtido);
+    }
+
+    @Test
+    void testInstanciaPadraoPoupanca(){
+        final Conta instance = new Conta();
+        assertFalse(instance.isPoupanca());
+    }
+
+    @Test
+    void testSetLimiteContaEspecial(){
+        final Conta instance = new Conta();
+        instance.setEspecial(true);
+        final double esperado = 1000;
+        instance.setLimite(esperado);
+        final double obtido = instance.getLimite();
+        assertEquals(esperado,obtido);
+    }
+    
+    @Test
+    void testSetLimiteContNaoEspecial(){
+        final Conta instance = new Conta();
+        final double limite = 1000;
+        assertThrows(IllegalStateException.class, () -> instance.setLimite(limite));
+    }
+
+    @Test
+    void testHistoricoNotNull(){
+        final Conta instance = new Conta();
+        assertNotNull(instance.getMovimentacoes());
+    }
+    //3.2 começa aqui:
+
+    @Test
+    void testGetSaldoTotal(){
+        final double limite = 500;
+        final double esperado = limite;
+        final Conta instance = new Conta();
+        instance.setEspecial(true);
+        instance.setLimite(limite);
+        final double obtido = instance.getSaldoTotal();
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testDepositoDinheiro(){
+        final double limite = 500, deposito = 500, esperado = 1000;
+        final Conta instance = new Conta();
+        instance.setEspecial(true);
+        instance.setLimite(limite);
+        instance.depositoDinheiro(deposito);
+
+        final double obtido = instance.getSaldoTotal();
+        assertEquals(esperado, obtido, 0.001);
+    }
+
+
+    
+}
